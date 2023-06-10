196be129309a292a1203838fafa865bac5194fb3
Neha <nehatualea@gmail.com>
2023-06-10 06:11:16 +0530

    google-docs-clone

---
+++ src/components/pages/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from './layout.module.css'
+import TextEditor from '../organism/TextEditor/TextEditor'
+
+const Layout = () => {
+  return (
+    <>
+    <div className={style.header}>Header</div>
+    <div className={style["edit-page"]}>
+        {/* <div className={style["edit-bar"]}></div> */}
+        <TextEditor />
+    </div>
+    </>
+    
+  )
+}
+
+export default Layout
